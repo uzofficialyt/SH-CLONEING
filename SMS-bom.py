78fe995da424096abd2a5ac4d209d3df2d9a58b8
shadhin-HR <124778109+Mdshadhinali@users.noreply.github.com>
2023-02-27 23:00:38 +0600

    Update SMS-bom.py

--- SMS-bom.py
+++ SMS-bom.py
@@ -206,17 +206,22 @@ def main():
         if (finished):
             break
 
+
+
+
+
+
 # Start Ruuning Tool
-
 if (__name__ == "__main__"):
-
-
-
+    checkPy()
+    logo()
     update()
-
     main()
 
-    Logo()
+
+
+
+
 
 
 
