a9193f0062669273cc75089077d22015b2cb4ca6
shadhin-HR <124778109+Mdshadhinali@users.noreply.github.com>
2023-03-10 01:40:27 +0600

    Update SHADHIN.py

--- SHADHIN.py
+++ SHADHIN.py
@@ -189,11 +189,17 @@ def gml():
         os.system('clear')
         print(logo)
         tl = str(len(user))
-        print(' \033[1;97m[+] Total ids:\033[1;92m '+tl)
-        print(' \033[1;97m[+] Process has been started')
-        print(' \033[1;97m[+] Wait for ids ')
-        print(' \033[1;97m[+] Use flight mode for speed up ')
-        print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        print('    \033[1;32m ┌───────────────────────────────")
+
+        print('     \033[1;95m│[+] Total ids:\033[1;92m '+tl)            
+
+        print('     \033[1;33m│[+] Process has been started')              
+
+        print('     \033[1;34m│[+] Wait for ids ')                          
+
+        print('     \033[1;91m│[+] Use flight mode for speed up ')          
+
+        print("    \033[1;32m └───────────────────────────────")
         for guru in user:
             uid = kode+kodex+guru+doamin
             pwx = [kode,kodex,kode+kodex,kode+'123',kode+'1234',kode+'12345',kode+guru,kodex+'123',kodex+'1234',kodex+'12345']
