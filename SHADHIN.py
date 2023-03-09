039dbca1ddb022a05e21eff02780196cb694e600
shadhin-HR <124778109+Mdshadhinali@users.noreply.github.com>
2023-03-10 01:55:19 +0600

    Update SHADHIN.py

--- SHADHIN.py
+++ SHADHIN.py
@@ -156,7 +156,7 @@ def num():
         print('     \033[1;34m│[+] Wait for ids ')                          
 
         print('     \033[1;91m│[+] Use flight mode for speed up ')          
-        print("     \033[1;91m│[×]NAMBAR-CLONING
+        print('     \033[1;91m│[×]NAMBAR-CLONING ')
         print("    \033[1;32m └──────────────────────────────────")
         for guru in user:
             uid = kode+kodex+kod+guru
@@ -198,7 +198,7 @@ def gml():
      
       
         print('     \033[1;91m│[+] Use flight mode for speed up ')
-        print('     \033[1;91m│[×]GMAIL-CLONING
+        print('     \033[1;91m│[×]GMAIL-CLONING ')
         print("    \033[1;32m └──────────────────────────────────")
         
                   
