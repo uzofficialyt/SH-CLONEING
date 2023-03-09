70a5da57cac2c76f67c13be837d3015a484708ff
shadhin-HR <124778109+Mdshadhinali@users.noreply.github.com>
2023-03-10 00:08:24 +0600

    Update SHADHIN.py

--- SHADHIN.py
+++ SHADHIN.py
@@ -150,11 +150,12 @@ def num():
         os.system('clear')
         print(logo)
         tl = str(len(user))
-        print(' \033[1;97m[+] Total ids:\033[1;92m '+tl)
-        print(' \033[1;97m[+] Process has been started')
-        print(' \033[1;97m[+] Wait for ids ')
-        print(' \033[1;97m[+] Use flight mode for speed up ')
-        print("\033[1;32m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+        print(' \033[1;97m┌───────────────────────────────────────┐
+        print(' \033[1;97m│[+] Total ids:\033[1;92m '+tl)         
+        print(' \033[1;97m│[+] Process has been started')
+        print(' \033[1;97m│[+] Wait for ids ')
+        print(' \033[1;97m│[+] Use flight mode for speed up ')
+        print("\033[1;32m └───────────────────────────────────────┘")
         for guru in user:
             uid = kode+kodex+kod+guru
             pwx = [kode+kodex+kod+guru,kod+guru,kodex+guru,kode+kodex+kod,]
