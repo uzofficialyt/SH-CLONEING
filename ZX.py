567cb84e4e713fee24c0c3946ecae57e3e10fa51
shadhin-HR <124778109+Mdshadhinali@users.noreply.github.com>
2023-03-11 00:20:41 +0600

    Update ZX.py

--- ZX.py
+++ ZX.py
@@ -1,12 +1,1 @@
-import os, sys
-
-os.system('xdg-open https://github.com/Mdshadhinali')
-
-try:
-
-    __import__("SHADHIN.cpython-311.opt-2.pyc").menu()
-
-except Exception as e:
-
-    exit(str(e))
 
