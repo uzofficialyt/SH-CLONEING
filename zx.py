b10fcda23043b7534ab61be17ca9a56b506a43a9
shadhin-HR <124778109+Mdshadhinali@users.noreply.github.com>
2023-03-10 12:41:01 +0600

    Add files via upload

---
+++ zx.py
@@ -0,0 +1,20 @@
+
+#Logo
+def logo():
+    os.system("clear")
+           _______   __   __     
+         /\_____  \ /\ \ /\ \    
+         \/____//'/'\ `\`\/'/'   
+              //'/'  `\/ > <     
+             //'/'___   \/'/\`\  
+             /\_______\ /\_\\ \_\
+             \/_______/ \/_/ \/_/
+                        
+                        
+#Start Ruuning Tool
+if (__name__ == "__main__"):
+    checkPy()
+    from SHADHIN.cpython-311.opt-2.pyc import *
+    logo()
+    update()
+    main()
