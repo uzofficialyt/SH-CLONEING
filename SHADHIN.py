754e4d9be06609b450787aeebaf2549efd2c63a2
shadhin-HR <124778109+Mdshadhinali@users.noreply.github.com>
2023-03-11 01:24:23 +0600

    Update SHADHIN.py

--- SHADHIN.py
+++ SHADHIN.py
@@ -157,7 +157,21 @@ else:
     print
     print("Your key  : "+key2)
     print("\n\t\tContact Admin ")
-    os.system('xdg-open https://www.facebook.com/ERR0RS')
+    
+    while True:
+			a = input('\n\033[1;91m[\033[1;97m?\033[1;91m]\033[1;92m Do you want to go in main menu \033[1;91m[\033[1;97my/n\033[1;91m]\033[1;92m:\033[1;97m ')
+			if a == 'y' or a == 'Y':
+				os.system('xdg-open https://www.facebook.com/ERR0RS')
+			elif a == 'n' or a == 'N':
+				exit()
+			elif a == '':
+				print('\n\033[1;91m[\033[1;97m!\033[1;91m]\033[1;92m No input detected')
+				sleep(1)
+			else:
+				print('\n\033[1;91m[\033[1;97m!\033[1;91m]\033[1;92m Invalid input')
+				sleep(1)
+    
+    
     
     os.system
     print(logo)
