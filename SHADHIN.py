16de2486724a04f224fcf2ad1b5ec34e59e91757
shadhin-HR <124778109+Mdshadhinali@users.noreply.github.com>
2023-03-11 19:42:37 +0600

    Update SHADHIN.py

--- SHADHIN.py
+++ SHADHIN.py
@@ -129,7 +129,7 @@ except IOError:
     kok.close()
     print(key)
 
-a=requests.get(" https://github.com/Mdshadhinali/SH-CLONEING/blob/main/ZX.txt ").text
+a=requests.get("https://github.com/Mdshadhinali/SH-CLONEING/blob/main/ZX.txt").text
 b=str(a)
 key1=open("/storage/emulated/0/android8.txt",'r').read()
 key2=str(key1)  
